e640238d9a3ea4905f82a9f65a6332e3b580050d
woyteka <40150050+woyteka@users.noreply.github.com>
2020-01-03 13:37:50 +0100

    menu02

--- js/script.js
+++ js/script.js
@@ -2,9 +2,29 @@ function main(){
     $(".menu-button-text").click(function() {
         $(".menu-container").css("display","flex");
       });
+
+      $(".exit-button").mouseover(function() {
+        $(".exit-one").css("background-color","#d74814");
+        $(".exit-two").css("background-color","#d74814");
+      });
+
+      $(".exit-button").mouseout(function() {
+        $(".exit-one").css("background-color","#f37446");
+        $(".exit-two").css("background-color","#f37446");
+      });
+      $(".exit-button").click(function() {
+        $(".menu-container").css("display","none");
+      });
+
+
  
  $('.content').load('strona-glowna.html .content');
      
+
+
+
+
+
     };
     window.onload=function(){
         main();
