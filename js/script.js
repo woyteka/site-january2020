3036b61b5de3822c2ebf09553635ad18a5d7d022
woyteka <40150050+woyteka@users.noreply.github.com>
2020-01-08 10:02:26 +0100

    add enter and exit menu animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 function main(){
-    $(".menu-button-text").click(function() {
-        $(".menu-container").css("display","flex");
-      });
+    
 
       $(".exit-button").mouseover(function() {
         $(".exit-one").css("background-color","#d74814");
@@ -12,10 +10,7 @@ function main(){
         $(".exit-one").css("background-color","#f37446");
         $(".exit-two").css("background-color","#f37446");
       });
-      $(".exit-button").click(function() {
-        $(".menu-container").css("display","none");
-      });
-
+     
 
  
  $('.content').load('strona-glowna.html .content');
@@ -35,5 +30,18 @@ function main(){
 
     };
     window.onload=function(){
+
+      $(".menu-button-text").click(function() {
+        $(".menu-container").removeClass("slider-menu-backwards");
+        $(".menu-container").addClass("slider-menu-play");
+      })
+
+
+
+      $(".exit-button").click(function() {
+        $(".menu-container").addClass("slider-menu-backwards");
+        $(".menu-container").removeClass("slider-menu-play");
+      })
+
         main();
     };
