e829ddecb0bad9c32ca0f494c142ad3ec4c0fce1
woyteka <40150050+woyteka@users.noreply.github.com>
2020-01-09 10:36:30 +0100

    animation links menu

--- js/script.js
+++ js/script.js
@@ -30,17 +30,31 @@ function main(){
 
     };
     window.onload=function(){
-
+      
       $(".menu-button-text").click(function() {
+        
         $(".menu-container").removeClass("slider-menu-backwards");
         $(".menu-container").addClass("slider-menu-play");
+        $(".sg").addClass("sg-anim");
+        $(".info").addClass("info-anim");
+        $(".portfolio").addClass("portfolio-anim");
+        $(".kontakt").addClass("kontakt-anim");
+      
       })
 
 
 
       $(".exit-button").click(function() {
+        
+        $(".sg").removeClass("sg-anim");
+        $(".info").removeClass("info-anim");
+        $(".portfolio").removeClass("portfolio-anim");
+        $(".kontakt").removeClass("kontakt-anim");
         $(".menu-container").addClass("slider-menu-backwards");
         $(".menu-container").removeClass("slider-menu-play");
+     
+        
+        
       })
 
         main();
